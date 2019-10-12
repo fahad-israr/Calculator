6fd0cf2a414a825f310349502b5d183a43690ece
Adithya12321 <56475239+Adithya12321@users.noreply.github.com>
2019-10-12 17:11:13 +0530

    bug fixed

--- calci.c
+++ calci.c
@@ -11,6 +11,10 @@ int main()
     if(c==1)printf("Sum is:%d\n",a+b);
     if(c==2)printf("Diff is:%d",a-b);
     if(c==3)printf("Product is:%d",a*b);
-    if(c==4)printf("Quotient is%d",a/b);
+    if(c==4)
+    {
+        if(b==0)printf("invalid");
+        else printf("Quotient is%d",a/b);
+    }
     return 0;
 }
